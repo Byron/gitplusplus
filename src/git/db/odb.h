2f87101ee7a5b5b0d1f900bb643e58ca3d08d01c
Sebastian Thiel <byronimo@gmail.com>
2011-02-18 00:34:42 +0100

    Moved implementation of serialization/deserialization back to header, as partial specialization never allows to put the code into an own compilation unit (to be compiled just once), as this is only possible if it is not dependent on any parameter anymore

--- src/git/db/odb.h
+++ src/git/db/odb.h
@@ -54,11 +54,46 @@ struct git_object_policy : public gtl::odb_object_policy<git_object_traits::inpu
 														 git_object_traits::output_reference_type>
 {
 	template <class StreamType>
-	void serialize(typename git_object_traits::input_reference_type object, StreamType& ostream);
-	
+	void serialize(typename git_object_traits::input_reference_type object, StreamType& ostream)
+	{
+		switch(object.type()) 
+		{
+			case Object::Type::Blob:
+			{
+				break;
+			}
+			default:
+			{
+				SerializationError err;
+				err.stream() << "invalid object type given for serialization: " << (uchar)object.type() << std::flush;
+				throw err;
+			}
+		}
+	}
 	
 	template <class ODBObjectType>
-	void deserialize(typename git_object_traits::output_reference_type out, const ODBObjectType& object);
+	void deserialize(typename git_object_traits::output_reference_type out, const ODBObjectType& object)
+	{
+		switch(object.type())
+		{
+			case Object::Type::Blob:
+			{
+				new (&out.blob) Blob;
+				out.blob.data().reserve(object.size());
+				std::unique_ptr<typename ODBObjectType::stream_type> pstream(object.new_stream());
+				boost::iostreams::back_insert_device<Blob::data_type> insert_stream(out.blob.data());
+				boost::iostreams::copy(*pstream, insert_stream);
+				break;
+			}
+			
+			default:
+			{
+				DeserializationError err;
+				err.stream() << "invalid object type given for deserialization: " << (uchar)object.type() << std::flush;
+				throw err;
+			}
+		}	
+	}
 };
 
 /** \ingroup ODB
